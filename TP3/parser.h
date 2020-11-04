1ee000098acd79463bbc50b792db85c4dbe266bc
Ignx <58445918+ignxal@users.noreply.github.com>
2020-11-03 22:31:10 -0300

    doxygen

--- TP3/parser.h
+++ TP3/parser.h
@@ -1,4 +1,35 @@
+/** \brief parsea un archivo de texto de empleado y lo agrega al linkedlist
+ *
+ * \param pFile FILE*
+ * \param pArrayListEmployee LinkedList*
+ *
+ * \return int: Return (-1) si NULL o (0) si 0k
+ */
 int parser_EmployeeFromText(FILE* pFile , LinkedList* pArrayListEmployee);
+
+/** \brief parsea un archivo binario de empleado y lo agrega al linkedlist
+ *
+ * \param pFile FILE*
+ * \param pArrayListEmployee LinkedList*
+ *
+ * \return int: Return (-1) si NULL o (0) si 0k
+ */
 int parser_EmployeeFromBinary(FILE* pFile , LinkedList* pArrayListEmployee);
+
+/** \brief escribe contenido de linked list en archivo de texto
+ *
+ * \param pFile FILE*
+ * \param pArrayListEmployee LinkedList*
+ *
+ * \return int: Return (-1) si NULL o (0) si 0k
+ */
 int parser_EmployeeToText(FILE* pFile , LinkedList* pArrayListEmployee);
+
+/** \brief escribe contenido de linked list en archivo binario
+ *
+ * \param pFile FILE*
+ * \param pArrayListEmployee LinkedList*
+ *
+ * \return int: Return (-1) si NULL o (0) si 0k
+ */
 int parser_EmployeeToBinary(FILE* pFile, LinkedList* pArrayListEmployee);
